5dce58c7b4ca0d0070680ded4eb1cf3d44bb915c
Minhajshaikh321 <shaikhminhaj8850@gmail.com>
2025-05-11 15:28:25 +0530

    regenrate html after update in checkout

--- AmazonProject/data/cart.js
+++ AmazonProject/data/cart.js
@@ -8,11 +8,14 @@ if(!cart){
 
     {
         productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity:2
+        quantity:2,
+        deliveryOptionId:'1'
     },
     {
         productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity:1
+        quantity:1,
+        deliveryOptionId:'2'
+
     },
 
   ];
@@ -40,7 +43,8 @@ export function addToCart(productId){
     else{
       cart.push({
         productId:productId,
-        quantity:1
+        quantity:1,
+        deliveryOptionId:'1'
       });
     }
     saveToStorage();
@@ -54,7 +58,7 @@ export function removeFromCart(productId){
     const newCart=[];
 
     cart.forEach((cartItem)=>{
-      if(cartItem.productId!==productId){
+      if(cartItem.productId !== productId){
         newCart.push(cartItem);
       }
     });
@@ -62,3 +66,17 @@ export function removeFromCart(productId){
     cart=newCart;
     saveToStorage();
 } 
+
+export function updateDeliveryOption(productId,deliveryOptionId){
+    let matchingItem;
+
+    cart.forEach((cartItem)=>{
+      if(productId === cartItem.productId){
+            matchingItem = cartItem;
+      }
+    });
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    saveToStorage();
+
+}
