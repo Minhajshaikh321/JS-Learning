35fce56457966ed8e3356284323b00edbd40c181
Minhajshaikh321 <shaikhminhaj8850@gmail.com>
2025-05-18 13:48:40 +0530

    convert loadproduct from xhr to fetch

--- AmazonProject/scripts/checkout.js
+++ AmazonProject/scripts/checkout.js
@@ -1,17 +1,82 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import {loadProducts} from '../data/products.js';
-
+import {loadProducts,loadProductsFetch} from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 // import '../data/cart-oop.js';
 // import '../data/cart-class.js';
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 
-loadProducts(()=>{
+//promise keepor code flat and avoid to much nesting;
+
+
+Promise.all([  //let just run multiple promise on same time
+
+    loadProductsFetch(),
+    new Promise((resolve)=>{ //wait for finish after resolve go to next function
+        loadCart(()=>{
+            resolve('value 2');
+        });
+    })
+
+]).then((value)=>{
+    console.log(value);
     renderOrderSummary();
     renderPaymentSummary();
-})
+});
 
-    // renderOrderSummary();
-    // renderPaymentSummary();
+/*
+Promise.all([  //let just run multiple promise on same time
 
+    new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve('value 1');  //shre the value to next func
+        });
+    }),
+    new Promise((resolve)=>{ //wait for finish after resolve go to next function
+        loadCart(()=>{
+            resolve('value 2');
+        });
+    })
+
+]).then((value)=>{
+    console.log(value);
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+
+*/
+
+/*
+new Promise((resolve)=>{
+    console.log('start promise 1');
+    loadProducts(()=>{
+        console.log('finish loading 1');
+        resolve('value 1');  //shre the value to next func
+    });
+}).then((value)=>{
+    console.log(value);
+    return new Promise((resolve)=>{ //wait for finish after resolve go to next function
+        console.log('start promise 2');
+        loadCart(()=>{
+            console.log('finish loading 2');
+            resolve();
+        });
+    });
+}).then(()=>{
+        console.log('finish loading 1ast func');
+        renderOrderSummary();
+        renderPaymentSummary();
+
+});
+*/
+
+/*
+loadProducts(()=>{
+    loadCart(()=>{
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+*/
 
